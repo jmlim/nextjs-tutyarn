9b76e91721fe6dd0d1fbe3cc1e045cd7d94782b7
jmlim <hackerljm@gmail.com>
2023-09-15 11:01:37 +0900

    Next js 셋팅 및  페이지 레이아웃 (Intro, Page layout)

---
+++ src/component/Top.js
@@ -0,0 +1,20 @@
+import { Header } from "semantic-ui-react";
+import Gnb from "./Gnb";
+
+export default function Top() {
+  return (
+    <div>
+      <div style={{ display: "flex", paddingTop: 20 }}>
+        <div style={{ flex: "100px 0 0" }}>
+          <img
+            src="/images/jmlim.png"
+            alt="logo"
+            style={{ display: "block", width: 80 }}
+          />
+          <Header as="h1">임정묵</Header>
+          <Gnb />
+        </div>
+      </div>
+    </div>
+  );
+}
