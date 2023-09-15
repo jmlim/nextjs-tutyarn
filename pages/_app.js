e8a0243ae99518146f5a6d24f04da263313c2fee
jmlim <hackerljm@gmail.com>
2023-09-15 11:01:58 +0900

    Next js 강좌 셋팅 및 페이지 레이아웃 (Intro, Page layout)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import Footer from "@/src/component/Footer";
+import Top from "@/src/component/Top";
+import "@/styles/globals.css";
+import "semantic-ui-css/semantic.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div style={{ width: 1000, margin: "0 auto" }}>
+      <Top />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div>카피라이트 어쩌구 푸터 임정묵</div>;
+}
