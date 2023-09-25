95e373fbf33db953281764ef9b844e51767021bf
jmlim <hackerljm@gmail.com>
2023-09-25 18:01:14 +0900

    정적 생성(Static Generation)

--- pages/index.js
+++ pages/index.js
@@ -7,51 +7,37 @@ import { Divider, Header, Loader } from "semantic-ui-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  function getData() {
-    axios.get(API_URL).then((res) => {
-      console.log(res.data);
-      setList(res.data);
-      setIsLoading(false);
-    });
-  }
-
-  useEffect(() => {
-    console.log("data");
-    getData();
-  }, []);
+export default function Home({ list }) {
   return (
     <div>
       <Head>
         <title>HOME| 임정묵</title>
         <meta name="description" content="임정묵 쇼핑몰입니다. "></meta>
       </Head>
-      {isLoading && (
-        <div style={{ padding: "300px 0" }}>
-          <Loader inline="centered" active>
-            Loading
-          </Loader>
-        </div>
-      )}
-      {!isLoading && (
-        <>
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            베스트 상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(0, 9)} />
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            신상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(9)} />
-        </>
-      )}
+      <>
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트 상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(0, 9)} />
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          신상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(9)} />
+      </>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
